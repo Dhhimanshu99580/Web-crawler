e835c2838dcceabe4957aad1ed94cd0bde27c4c9
Himanshu <himanshu99580@gmail.com>
2024-01-07 00:56:37 +0530

    enhancement

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { argv } = require('node:process')
 const {crawlPage} = require('./crawl.js')
-
+const {printReport} = require('./report.js')
 
 async function main() {
   if(argv.length <=2 || argv.length>3) {
@@ -9,8 +9,9 @@ async function main() {
     console.log("Hold on...I will start the crawl for..",argv[2])
     let pages = {}
     pages = await crawlPage(argv[2],argv[2],pages)
-    console.log(pages)
-    console.log(Object.keys(pages).length)
+    //console.log(pages)
+    //console.log(Object.keys(pages).length)
+    printReport(pages)
   }
 }
 
--- report.js
+++ report.js
@@ -1,4 +1,19 @@
 function printReport(pages) {
     console.log("Report is getting ready......")
-    
+    const keyValueArray = Object.entries(pages);
+
+// Sort the array in descending order based on values using a regular function
+    keyValueArray.sort(function(a, b) {
+    return b[1] - a[1];
+    })
+
+     pages = Object.fromEntries(keyValueArray)
+
+    for(const key in pages) {
+        console.log(`Found ${pages[key]} internal links to ${key}`)
+    }
+}
+
+module.exports = {
+    printReport
 }
